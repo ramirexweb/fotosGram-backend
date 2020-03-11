0ff4a9ae58b5ee7945dfc6985191eec596e8e21d
ramirex <ramirex@gmail.com>
2020-03-11 17:33:10 -0400

    implementando mongoose

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
 import Server from './classes/server';
+import userRoutes from './routes/usuario.routes';
+import mongoose from 'mongoose';
+import 'colorts/lib/string';
 
 
 const server = new Server();
 
+// Rutas app
+server.app.use('/user', userRoutes);
+
+// Conectarr DB
+mongoose.connect('mongodb://localhost:27017/fotosgram', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (err) => {
+    if ( err ) throw err;
+
+    console.log('Base de datos ONLINE'.yellow);
+});
 
 // init express
 server.start( () => {
