79825c4d7d0694542d705708860d8e366ce6fb58
ramirex <ramirex@gmail.com>
2020-03-13 10:29:02 -0400

    creat token

--- routes/usuario.routes.ts
+++ routes/usuario.routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { Usuario } from '../models/usuario.model';
 import bcrypt from 'bcrypt';
+import Token from '../classes/token';
+import { verificarToken } from '../middlewares/autenticacion';
 
 const userRoutes = Router();
 
@@ -20,10 +22,18 @@ userRoutes.post('/login', ( req: Request, res: Response) => {
             });
         }
 
-        if ( userDB?.compararPassword( body.password)) {
+        if ( userDB.compararPassword( body.password)) {
+
+            const tokenUser = Token.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+
             res.json({
                 ok: true,
-                token: '432324324423DSAKJDADAJKDHASDKJHKJHKJHH'
+                token: tokenUser
             });
         } else {
             return res.json({
@@ -46,9 +56,17 @@ userRoutes.post('/create', ( req: Request, res: Response) => {
 
     Usuario.create( user)
         .then( userDB => {
+
+            const tokenUser = Token.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+
             res.json({
                 ok: true,
-                user: userDB
+                token: tokenUser
             });
         })
         .catch( err => {
@@ -57,6 +75,42 @@ userRoutes.post('/create', ( req: Request, res: Response) => {
                 err
             })
         });
+});
+
+userRoutes.post('/update', verificarToken, (req: any, res: Response) => {
+
+    const user = {
+        nombre: req.body.nombre || req.usuario.nombre,
+        email: req.body.email || req.usuario.email,
+        avatar: req.body.avatar || req.usuario.avatar
+    }
+
+    Usuario.findByIdAndUpdate( req.usuario._id, user, {
+        new: true
+    }, ( err, userDB) => {
+        if ( err ) throw err;
+
+        if ( !userDB ) {
+            return res.json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese ID'
+            });
+        }
+
+        const tokenUser = Token.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
+        });
+
+        res.json({
+            ok: true,
+            token: tokenUser
+        });
+
+
+    });
 });
 
 export default userRoutes;
